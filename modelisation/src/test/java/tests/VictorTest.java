2a43559c9cd60137e1a976c86aba9984f0fec1ff
Victor Demeestere <victor.demeestere.etu@univ-lille.fr>
2020-10-16 15:44:19 +0200

    ouverture du fichier fxml

--- modelisation/src/test/java/tests/VictorTest.java
+++ modelisation/src/test/java/tests/VictorTest.java
@@ -1,14 +1,28 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-import org.junit.jupiter.api.Test;
 
-class VictorTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+public class VictorTest extends Application {  
+	   @Override     
+	   public void start(Stage primaryStage) throws Exception { 
+           FXMLLoader loader = new FXMLLoader(getClass().getResource("test.fxml"));
+           Parent root = loader.load();
 
-}
+           Scene scene = new Scene(root);
+           primaryStage.setTitle("Accueil");
+           primaryStage.setScene(scene);
+           primaryStage.setResizable(false);
+           primaryStage.show();
+	   }         
+	   public static void main(String args[]){           
+	      launch(args);      
+	   } 
+	} 
