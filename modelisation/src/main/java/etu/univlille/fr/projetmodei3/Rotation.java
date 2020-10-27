42fb1bb443020226aa06a03c2c4ffc12a5da821f
guilamb <guilhanebourgin@gmail.com>
2020-10-27 15:40:17 +0100

    enlevé javafx de rotation

--- modelisation/src/main/java/etu/univlille/fr/projetmodei3/Rotation.java
+++ modelisation/src/main/java/etu/univlille/fr/projetmodei3/Rotation.java
@@ -16,117 +16,15 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-/*TODO
- * Il faut retirer javafx
- */
 
 
 
-public class Rotation extends Application{
+
+public class Rotation{
 	
 	boolean rotation = false;
 	int angle = 1;
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	public void start(Stage primaryStage) {
 		
-		Pane pane = new Pane();
-		Scene scene = new Scene(pane,800,500);
-		
-		Rectangle carre = new Rectangle();
-		carre.setX(100);
-		carre.setY(100);
-		carre.setFill(Paint.valueOf("black"));
-		carre.setWidth(300.0f); 
-	    carre.setHeight(150.0f);
-	    
-	    
-	    Point point0 = new Point(50, 50, 0);
-	    Point point1 = new Point(100, 50, 0 );
-	    Point point2 = new Point(100, 100, 0);
-	    Point point3 = new Point(50, 100, 0);
-	    Point point4 = new Point(50, 50, 100);
-	    Point point5 = new Point(100, 50, 100);
-	    Point point6 = new Point(100, 100, 100);
-	    Point point7 = new Point(50, 100, 100);
-	    
-	    Circle p1 = new Circle(point0.getX(),point0.getY(),10);
-	    Circle p2 = new Circle(point1.getX(), point1.getY(),10);
-	    Circle p3 = new Circle(point2.getX(), point2.getY(),10);
-	    Circle p4 = new Circle(point3.getX(), point3.getY(),10);
-	    Circle p5 = new Circle(point4.getX(), point4.getY(),10);
-	    Circle p6 = new Circle(point5.getX(), point5.getY(),10);
-	    Circle p7 = new Circle(point6.getX(), point6.getY(),10);
-	    Circle p8 = new Circle(point7.getX(), point7.getY(),10);
-
-	    
-	    //Box cube = new Box(1,1,1);
-		
-	    Thread thread;
-		Button tap = new Button("rotation 90");
-		
-		Runnable runnable = new Runnable() {
-			
-			@Override
-			public void run() {
-				rotate(carre, angle);
-				rotate(p1,point0,angle);
-				rotate(p2,point1,angle);
-				rotate(p3,point2,angle);
-				rotate(p4,point3,angle);
-				rotate(p5,point4,angle);
-				rotate(p6,point5,angle);
-				rotate(p7,point6,angle);
-				rotate(p8,point7,angle);
-
-
-
-				//carre.setX(carre.getX()+angle);
-				angle+=1;
-				System.out.println("loop");
-
-				try {
-					Thread.sleep(1000);
-					Thread.currentThread().run();
-					System.out.println("ok");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-			}
-		};
-		
-		thread = new Thread(runnable);
-
-		tap.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				
-				thread.start();
-				
-				
-			}
-		});
-		
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-		    @Override
-		    public void handle(WindowEvent t) {
-		        Platform.exit();
-		        System.exit(0);
-		    }
-		});
-
-		
-		pane.getChildren().addAll(/*carre,*/tap,p1,p2,p3,p4,p5,p6,p7,p8);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
-		
-	}
 	public void rotate(Rectangle carre, int degree) {
 		double X=(carre.getX()*Math.cos(degree)-carre.getY()*Math.sin(degree));
 		double Y=(carre.getX()*Math.sin(degree)+carre.getY()*Math.cos(degree));
@@ -168,14 +66,6 @@ public class Rotation extends Application{
 		
 		Matrix m = new Matrix(new Point(x,y,z));
 		return m;
-	}
-	public Matrix translate(Matrix m) {//marche pas
-		double[][] tmp = 
-			{{1, 0, -50},
-			{0, 1, -50},
-			{0, 0, 1}};
-		
-		return new Matrix(tmp).Multiply(m);
 	}
 	public Matrix project(Matrix m) {
 		double[][] tmp = 
