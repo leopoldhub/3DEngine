4e334c9030bc1fe05d3da68579e1f6eded7b12e1
Maxime <Maxime@10.135.6.10>
2020-11-08 20:14:50 +0100

    update Main

--- modelisation/src/main/java/etu/univlille/fr/projetmodei3/Main.java
+++ modelisation/src/main/java/etu/univlille/fr/projetmodei3/Main.java
@@ -1,14 +1,21 @@
 package etu.univlille.fr.projetmodei3;
 
+import java.io.File;
+
+import etu.univlille.fr.projetmodei3.interfaces.ControllerDesImpots;
 import etu.univlille.fr.projetmodei3.objects.Face;
 import etu.univlille.fr.projetmodei3.objects.Model3D;
 import etu.univlille.fr.projetmodei3.objects.ModelViewer;
+import etu.univlille.fr.projetmodei3.objects.Parser;
 import etu.univlille.fr.projetmodei3.objects.Point;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,7 +27,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Point p1,p2,p3,p4,p5;
+	Point p1,p2,p3,p4,p5;
 		Face f1,f2,f3,f4,f5;
 		Model3D obj;
 		
@@ -42,24 +49,42 @@ public class Main extends Application{
 		f5 = new Face(new Point[] {p3,p1,p5});
 		
 		obj = new Model3D(new Face[] {f1,f2,f3,f4,f5});
-			
 		
+		System.out.println("Debut de parse");
+		//obj = Parser.parse(new File(System.getProperty("user.dir")+"/src/main/resources"+File.separator+"Crane.ply"));
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/affichage.fxml"));
+		Parent root = loader.load();
+		System.out.println(root.getChildrenUnmodifiable().get(0));
+		
+		/*
 		ModelViewer vue = new ModelViewer(obj);
+		vue.setHeight(450);
+		vue.setWidth(900);
+		System.out.println("debut de translate");
+		vue.translate(200,200,200);
+		System.out.println("Debut de zoom");
+		vue.zoom(10);
+		System.out.println("affichage");
 		vue.affichage();
-
-		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), ev -> {
-			vue.rotate(1.0/360.0,1.0/360.0,1.0/360.0);
+		
+		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), ev -> {
+			//vue.rotate(10.0/360.0,10.0/360.0,10.0/360.0);
+			//vue.zoom(1.01);
+			//vue.translate(1,1,1);
 			//System.out.println(object.getFaces());
-			vue.affichage();
+			//vue.affichage();
 		}));
+		
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.play();
-		
-		Scene scene = new Scene(vue);
+		*/
+		//AnchorPane ap = new AnchorPane(vue);
+		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-		
+
 	}
 
 	
