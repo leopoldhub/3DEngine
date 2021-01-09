984dede3e5d6680726bcc7fd24b7c00b84974eaa
Maxime <Maxime@10.135.4.51>
2021-01-09 18:28:24 +0100

    Javadoc : Trieur et méthodes de tri

--- modelisation/src/main/java/etu/univlille/fr/projetmodei3/objects/TriParNombreFace.java
+++ modelisation/src/main/java/etu/univlille/fr/projetmodei3/objects/TriParNombreFace.java
@@ -5,20 +5,35 @@ import java.util.Collections;
 import java.util.List;
 
 import etu.univlille.fr.projetmodei3.interfaces.Tri;
-
+/**
+ * Classe faisant partie du Patron strategy, permettant le tri des fichiers par leur nombre de Faces
+ * implémente l'interface Tri
+ * @author Leopold HUBERT, Maxime BOUTRY, Guilhane BOURGOING, Luca FAUBOURG
+ *
+ */
 public class TriParNombreFace implements Tri{
 
+	/**
+	 * Surcharge de la methode compare issue de l'interface comparator
+	 */
 	@Override
 	public int compare(File o1, File o2) {
 		return Parser.parseNb(o1,"face")-Parser.parseNb(o2,"face");
 	}
 
+	/**
+	 * Methode de Tri d'une liste de modele .ply
+	 * Issue de l'interface Tri
+	 */
 	@Override
 	public void trier(List<File> files) {
 		Collections.sort(files,this);
 	}
 	
 	
+	/**
+	 * Affichage du type de tri, utilisé dans Affichage.java pour afficher la méthode de tri courante
+	 */
 	public String toString() {
 		return "nb face";
 	}
--- modelisation/src/main/java/etu/univlille/fr/projetmodei3/objects/TriParNombrePoints.java
+++ modelisation/src/main/java/etu/univlille/fr/projetmodei3/objects/TriParNombrePoints.java
@@ -6,18 +6,34 @@ import java.util.List;
 
 import etu.univlille.fr.projetmodei3.interfaces.Tri;
 
+/**
+ * Classe faisant partie du Patron strategy, permettant le tri des fichiers par leur nombre de Points
+ * implémente l'interface Tri
+ * @author Leopold HUBERT, Maxime BOUTRY, Guilhane BOURGOING, Luca FAUBOURG
+ *
+ */
 public class TriParNombrePoints implements Tri{
 
+	/**
+	 * Surcharge de la methode compare issue de l'interface comparator
+	 */
 	@Override
 	public int compare(File o1, File o2) {
 		return Parser.parseNb(o1,"vertex")-Parser.parseNb(o2,"vertex");
 	}
 
+	/**
+	 * Methode de Tri d'une liste de modele .ply
+	 * Issue de l'interface Tri
+	 */
 	@Override
 	public void trier(List<File> files) {
 		Collections.sort(files,this);
 	}
 
+	/**
+	 * Affichage du type de tri, utilisé dans Affichage.java pour afficher la méthode de tri courante
+	 */
 	public String toString() {
 		return "nb points";
 	}
