77c046b3b1796ed0e4923985f8ac3c0fdf4c2186
guilamb <guilhanebourgin@gmail.com>
2021-01-09 17:46:43 +0100

    javadoc de fd

--- modelisation/src/main/java/etu/univlille/fr/projetmodei3/objects/FileDescriptor.java
+++ modelisation/src/main/java/etu/univlille/fr/projetmodei3/objects/FileDescriptor.java
@@ -12,23 +12,35 @@ public class FileDescriptor {
 	public StringProperty face;
 	public StringProperty vertex;
 	private File f;
-	
+	/**
+	 * Constructeur basique qui crée un descripteur de fichier à partir du nom du fichier
+	 * @param name the name of the file
+	 */
 	public FileDescriptor(String name) {
 		this.name = new SimpleStringProperty(name);
 	}
 	
-	
+	/**
+	 * Utilisé pour la TableView
+	 * @return le nom du fichier
+	 */
 	public StringProperty NameProperty() {
 		if (name == null) name = new SimpleStringProperty(this, "name");
         return name; 
 	}
 
-
+	/**
+	 * 
+	 * @return le nom
+	 */
 	public String getName() {
 		return name.get();
 	}
 
-
+/**
+ * 
+ * @param name le nom du fichier
+ */
 	public void setName(String name) {
 		this.name = new SimpleStringProperty(name);
 	}
@@ -37,13 +49,19 @@ public class FileDescriptor {
 
 
 
-	
+	/**
+	 * Utilisé pour la TableView
+	 * @return creator
+	 */
 	public StringProperty CreatorProperty() {
 		if (creator == null) creator = new SimpleStringProperty(this, "Creator");
         return creator; 
 	}
 
-
+	/**
+	 * 
+	 * @return le createur du fichier si précisé sinon none
+	 */
 	public String getCreator() {
 		String tmp;
 		try {
@@ -54,7 +72,10 @@ public class FileDescriptor {
 		return tmp;
 	}
 
-
+/**
+ * 
+ * @param creator le créateur du fichier
+ */
 	public void setCreator(String creator) {
 		this.creator = new SimpleStringProperty(creator);
 	}
@@ -64,18 +85,27 @@ public class FileDescriptor {
 
 
 
-
+	/**
+	 * Utilisé pour la TableView
+	 * @return face
+	 */
 	public StringProperty FaceProperty() {
 		if (face == null) face = new SimpleStringProperty(this, "Face");
         return face; 
 	}
 
-
+/**
+ * 
+ * @return le nombre de faces
+ */
 	public String getFace() {
 		return face.get();
 	}
 
-
+/**
+ * 
+ * @param face, le nombre de faces
+ */
 	public void setFace(String face) {
 		this.face = new SimpleStringProperty(face);
 	}
@@ -83,26 +113,41 @@ public class FileDescriptor {
 
 
 
-
+	/**
+	 * Utilisé pour la TableView
+	 * @return vertex
+	 */
 	public StringProperty VertexProperty() {
 		if (vertex == null) vertex = new SimpleStringProperty(this, "Vertex");
         return vertex; 
 	}
 
-
+/**
+ * 
+ * @return vertex, le nombre de sommets
+ */
 	public String getVertex() {
 		return vertex.get();
 	}
 
-
+/**
+ * 
+ * @param vertex, le nombre de sommets
+ */
 	public void setVertex(String vertex) {
 		this.vertex = new SimpleStringProperty(vertex);
 	}
-	
+/**
+ * 	
+ * @param f, le fichier ply
+ */
 	public void setFile(File f) {
 		this.f = f;
 	}
-	
+/**
+ * 	
+ * @return f, le fichier ply
+ */
 	public File getFile() {
 		return this.f;
 	}
