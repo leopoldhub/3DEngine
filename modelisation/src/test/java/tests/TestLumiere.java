e76d7767b7ebc83d5dd5bb8b52fa79b5ce2774cc
Maxime <Maxime@10.135.4.51>
2021-01-10 22:55:50 +0100

    Ajout des pages HTML javadoc

--- modelisation/src/test/java/tests/TestLumiere.java
+++ modelisation/src/test/java/tests/TestLumiere.java
@@ -1,11 +1,14 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import etu.univlille.fr.projetmodei3.objects.Face;
 import etu.univlille.fr.projetmodei3.objects.Model3D;
 import etu.univlille.fr.projetmodei3.objects.Point;
+import etu.univlille.fr.projetmodei3.utils.MathsUtils;
 /**
  * classe de test de la lumière
  * @author grp I3
@@ -15,7 +18,7 @@ public class TestLumiere {
 	/**
 	 * les points du modele
 	 */
-	private Point p1,p2,p3,p4,p5;
+	private Point p1,p2,p3,p4,p5,v12,v34,v45;
 	/**
 	 * les faces du modele
 	 */
@@ -32,10 +35,15 @@ public class TestLumiere {
 	private void setup() {
 		
 		p1 = new Point(-1, 0, -1);
-		p2 = new Point( 1, 0, -1);
+		p2 = new Point( 1, 0, -1); 
 		p3 = new Point( 1, 0,  1);
-		p4 = new Point(-1, 0, -1);
+		p4 = new Point(-1, 0, 1); 
 		p5 = new Point( 0, 2,  0);
+		
+		v12 = MathsUtils.getVecteur(p1, p2); //2,0,0
+		v34 = MathsUtils.getVecteur(p3, p4); //2,0,0
+		v45 = MathsUtils.getVecteur(p4, p5); //1,2,-1
+		
 		
 		f1 = new Face(new Point[] {p1,p2,p3,p4});
 		f2 = new Face(new Point[] {p1,p2,p5});
@@ -43,13 +51,17 @@ public class TestLumiere {
 		f4 = new Face(new Point[] {p3,p4,p5});
 		f5 = new Face(new Point[] {p4,p1,p5});
 		
+		
+		
 		obj = new Model3D(new Face[] {f1,f2,f3,f4,f5});
 	}
 	
 	@Test
 	
 	void testProduitVectoriel() {
-		
+		assertEquals(MathsUtils.produitVectoriel(v12,v34),new Point(0,-0,0));
+		assertEquals(MathsUtils.produitVectoriel(v34,v34),new Point(0,0,0));
+		assertEquals(MathsUtils.produitVectoriel(v34,v45),new Point(0,2,4));
 	}
 
 	@Test
